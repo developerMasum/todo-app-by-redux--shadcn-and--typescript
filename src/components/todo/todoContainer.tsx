af23b213c4c7fd0c4c34f83b537372e3c4a21569
developerMasum <developer.masum2001@gmail.com>
2024-01-15 10:18:07 +0600

    added fix container

--- src/components/todo/todoContainer.tsx
+++ src/components/todo/todoContainer.tsx
@@ -12,16 +12,23 @@ const TodoContainer = () => {
         <AddToDoModal />
         <TodoFilter />
       </div>
-      <div className=" bg-primary-gradient w-full h-full rounded-xl   p-[5px]">
-        <div className="bg-white w-full p-5 h-full rounded-lg space-y-3 ">
-          {todos.map((item) => (
-            <TodoCard {...item} />
-          ))}
+     
+      {todos && todos.length > 0 ? (
+        <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
+          <div className="bg-white w-full p-5 h-full rounded-lg space-y-3">
+         
+            {todos.map((item, index) => (
+              <TodoCard key={index} {...item} />
+            ))}
+          </div>
         </div>
-        {/* <div className="bg-white p-3 flex justify-center items-center text-2xl font-bold ">
-               <p className=""> There is no Task pending </p>
-             </div> */}
-      </div>
+      ) : (
+        <div className="bg-white p-5 flex justify-center items-center text-2xl font-bold">
+          <p className="border  border-cyan-500  p-5 rounded-lg uppercase text-lg">
+            There is no task pending. Please click on ADD TODO.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
